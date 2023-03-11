d936174d23df878abf23a9a0cdf7d28535b6b3f5
Николай Слесарев <kolansers@yandex.ru>
2023-03-11 14:28:58 +0300

    Реализован парсинг всех страниц.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-def url_next_page(markup, url_main):
+def url_next_page(soup, url_main):
     """Проверка на наличие следующей страницы."""
-    soup = BeautifulSoup(markup, 'lxml')
     pagination = soup.find('div', class_='pagination').find_all('li')[-1]
     if pagination.find('a').get('class') == ['arrow']:
         return url_main + pagination.find('a').get('href')
@@ -15,11 +14,15 @@ def get_one_scrap(url, headers):
     soup = BeautifulSoup(response.text, 'lxml')
     start_position = 1
     table_rows = soup.find('table', class_='problems').find_all('tr')[start_position:]
+    url_main = url.split('/problemset')[0]
     one_scrap = []
     for table_row in table_rows:
         data = []
         row_cells = table_row.find_all('td')
-        data.append(row_cells[0].find('a').text.strip())
+        number = row_cells[0].find('a').text.strip()
+        href = url_main + row_cells[0].find('a').get('href')
+        data.append(number)
+        data.append(href)
         name_tags = row_cells[1].find_all('div')
         name = name_tags[0].find('a').text.strip()
         data.append(name)
@@ -34,16 +37,24 @@ def get_one_scrap(url, headers):
             resolved = row_cells[4].find('a').text.strip()[1:]
         data.append(resolved)
         one_scrap.append(data)
-    return one_scrap
+    is_url_next_page = url_next_page(soup, url_main)
+    return one_scrap, is_url_next_page
+
+
+def scrap(url, headers):
+    """Парсинг всех страниц."""
+    is_url_next_page = url
+    while is_url_next_page:
+        one_scrap, is_url_next_page = get_one_scrap(is_url_next_page, headers)
+        print(one_scrap[0], sep='\n')
+
 
 def main():
-    url = 'https://codeforces.com/problemset/page/86?order=BY_SOLVED_DESC'
+    url = 'https://codeforces.com/problemset/page/1?order=BY_SOLVED_DESC'
     headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
     }
-    get_one_scrap(url, headers)
+    scrap(url, headers)
 
 
 if __name__=='__main__':
