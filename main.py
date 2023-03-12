969a0b65fc0392719dfb2a6f950cee83f681db20
Николай Слесарев <kolansers@yandex.ru>
2023-03-12 14:41:22 +0300

    Сделан доступ к БД через env

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
+from db import insert_data
+from os import getenv
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+RETRY_TIME = 3600
+
 
 def url_next_page(soup, url_main):
     """Проверка на наличие следующей страницы."""
@@ -13,29 +23,31 @@ def get_one_scrap(url, headers):
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'lxml')
     start_position = 1
-    table_rows = soup.find('table', class_='problems').find_all('tr')[start_position:]
+    table_rows = soup.find(
+        'table', class_='problems'
+    ).find_all('tr')[start_position:]
     url_main = url.split('/problemset')[0]
     one_scrap = []
     for table_row in table_rows:
         data = []
         row_cells = table_row.find_all('td')
         number = row_cells[0].find('a').text.strip()
-        href = url_main + row_cells[0].find('a').get('href')
+        url_task = url_main + row_cells[0].find('a').get('href')
         data.append(number)
-        data.append(href)
+        data.append(url_task)
         name_tags = row_cells[1].find_all('div')
         name = name_tags[0].find('a').text.strip()
         data.append(name)
-        tags = [tag.text.strip() for tag in name_tags[1].find_all('a')]
-        data.append(tags)
-        complexity = None
-        if row_cells[3].find('span'):  
-            complexity = row_cells[3].find('span').text.strip()
+        complexity = 0
+        if row_cells[3].find('span'):
+            complexity = int(row_cells[3].find('span').text.strip())
         data.append(complexity)
         resolved = None
         if row_cells[4].find('a'):
-            resolved = row_cells[4].find('a').text.strip()[1:]
+            resolved = int(row_cells[4].find('a').text.strip()[1:])
         data.append(resolved)
+        tags = [tag.text.strip() for tag in name_tags[1].find_all('a')]
+        data.append(tags)
         one_scrap.append(data)
     is_url_next_page = url_next_page(soup, url_main)
     return one_scrap, is_url_next_page
@@ -43,19 +55,29 @@ def get_one_scrap(url, headers):
 
 def scrap(url, headers):
     """Парсинг всех страниц."""
+    host = getenv('HOST')
+    user = getenv('USER')
+    password = getenv('PASSWORD')
+    db_name = getenv('DB_NAME')
     is_url_next_page = url
     while is_url_next_page:
-        one_scrap, is_url_next_page = get_one_scrap(is_url_next_page, headers)
-        print(one_scrap[0], sep='\n')
+        print(is_url_next_page)
+        one_scrap, is_url_next_page = get_one_scrap(
+            is_url_next_page,
+            headers
+        )
+        insert_data(host, user, password, db_name, one_scrap)
 
 
 def main():
-    url = 'https://codeforces.com/problemset/page/1?order=BY_SOLVED_DESC'
+    url = 'https://codeforces.com/problemset/page/70?order=BY_SOLVED_DESC'
     headers = {
         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
     }
-    scrap(url, headers)
+    while True:
+        scrap(url, headers)
+        sleep(RETRY_TIME)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
