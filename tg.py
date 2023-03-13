73557534c9f545cf73a47d249d51bcac2ab547dc
Николай Слесарев <kolansers@yandex.ru>
2023-03-13 21:28:26 +0300

    Добавлено для бота выдача тем для пользователя

--- tg.py
+++ tg.py
@@ -1,38 +1,39 @@
-import logging
 import os
 
 from dotenv import load_dotenv
 from telegram.ext import CommandHandler, Updater, Filters, MessageHandler
-from db import select_tasks
+from db import select_tasks, select_tags_uniq
 
 load_dotenv()
 
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
-UPDATER = Updater(token=TELEGRAM_TOKEN)
+HOST = os.getenv('HOST')
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
 
 
 def start(update, context):
     chat = update.effective_chat
+    tags_uniq = select_tags_uniq(HOST, USER, PASSWORD, DB_NAME)
+    tags_uniq_mes = '\n'.join(map(lambda elem: elem[0], tags_uniq))
     context.bot.send_message(
         chat_id=chat.id,
         text=(
-            'Введите тему и сложность в формате: тема-сложность.'
-            ' Например: математика-800.'
+            'Введите тему и сложность в формате: тема-сложность.\n'
+            'Например: математика-800.\n'
+            f'У нас есть задачи по темам:\n'
+            f'{tags_uniq_mes}'
         )
     )
 
 
 def get_tasks(update, context):
-    host = os.getenv('HOST')
-    user = os.getenv('USER')
-    password = os.getenv('PASSWORD')
-    db_name = os.getenv('DB_NAME')
     tag = update.message.text.split('-')[0]
     complexity = update.message.text.split('-')[1]
-    select = select_tasks(host, user, password, db_name, tag, complexity)
+    select = select_tasks(HOST, USER, PASSWORD, DB_NAME, tag, complexity)
     chat = update.effective_chat
     context.bot.send_message(
         chat_id=chat.id,
@@ -41,15 +42,16 @@ def get_tasks(update, context):
 
 
 def main():
-    UPDATER.dispatcher.add_handler(CommandHandler('start', start))
-    UPDATER.dispatcher.add_handler(
+    updater = Updater(token=TELEGRAM_TOKEN)
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(
         MessageHandler(
             Filters.regex(r'^[а-я]+-[\d]+'),
             get_tasks
         )
     )
-    UPDATER.start_polling()
-    UPDATER.idle()
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
